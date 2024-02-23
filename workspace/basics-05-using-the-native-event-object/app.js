2014a7837b515b67f22d842c649d703ac6f7f5fd
Jaemyung <p.jaemyung91@gmail.com>
2024-02-23 15:22:44 +0900

    FEAT: Learn abount style, css of Vue

--- workspace/basics-05-using-the-native-event-object/app.js
+++ workspace/basics-05-using-the-native-event-object/app.js
@@ -2,12 +2,45 @@ const app = Vue.createApp({
   data() {
     return {
       counter: 0,
-      name: ''
+      name: "",
+      lastName: "",
+      // fullname: "",
     };
   },
+  watch: {
+    counter(value) {
+      if(value > 50) {
+        const that = this;
+        setTimeout(function() {
+          that.counter = 0;
+        }, 2000);
+      }
+    }
+    // name(value) {
+    //   if (value === "") this.fullname = "";
+    //   else this.fullname = value + " " + this.lastName;
+    // },
+    // lastName(value) {
+    //   if (value === "") this.fullname = "";
+    //   else this.fullname = this.name + " " + value;
+    // },
+  },
+  computed: {
+    // fullname() {
+      // if (this.name === "" || this.lastName === "") return "";
+      // return this.name + " " + this.lastName;
+    // }
+  },
   methods: {
-    setName(event, lastName) {
-      this.name = event.target.value + ' ' + lastName;
+    outputFullname() {
+      if (this.name === "") return "";
+      return this.name + " " + "Park";
+    },
+    resetInput() {
+      this.name = "";
+    },
+    setName(event) {
+      this.name = event.target.value;
     },
     add(num) {
       this.counter = this.counter + num;
@@ -15,8 +48,8 @@ const app = Vue.createApp({
     reduce(num) {
       this.counter = this.counter - num;
       // this.counter--;
-    }
-  }
+    },
+  },
 });
 
 app.mount('#events');
