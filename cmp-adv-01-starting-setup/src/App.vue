0aaf14d599124b7cb8fa7bd4fd97fa37ad843d3f
Jaemyung <p.jaemyung91@gmail.com>
2024-03-05 14:39:02 +0900

    FEAT: Learn about details of Component like handling scope, slot, teleport, etc...

---
+++ cmp-adv-01-starting-setup/src/App.vue
@@ -0,0 +1,75 @@
+<template>
+  <div>
+    <!-- <the-header></the-header> -->
+    <TheHeader />
+    <!-- <badge-list></badge-list>
+    <user-info
+      :full-name="activeUser.name"
+      :info-text="activeUser.description"
+      :role="activeUser.role"
+    ></user-info>
+    <course-goals #default="slotProps">
+       <template #default="slotProps"> -->
+        <!-- slot에 default밖에 없을 때는 template 삭제 가능 -->
+        <!-- <h2>{{ slotProps.item }}</h2>
+        <p>{{ slotProps }}</p>
+        <p>{{ slotProps.anotherProp }}</p> -->
+        <!-- 강의에서는 slotProps['another-prop']으로 썼는데 slotProps prop확인했을 때 anotherProp으로 넘어와서 이렇게 씀, 버전 업데이트에 따른 변경사항인듯 -->
+       <!-- </template> -->
+    <!-- </course-goals> -->
+    <button @click="setSelectedComponent('active-goals')">Active Goals</button>
+    <button @click="setSelectedComponent('manage-goals')">Manage Goals</button>
+    <!-- <active-goals v-if="selectedComponent === 'active-goals'"></active-goals>
+    <manage-goals v-if="selectedComponent === 'manage-goals'"></manage-goals> -->
+    <keep-alive>
+      <component :is="selectedComponent"></component>
+    </keep-alive>
+  </div>
+</template>
+
+<script>
+import TheHeader from './components/layout/TheHeader.vue';
+// import BadgeList from './components/BadgeList.vue';
+// import UserInfo from './components/UserInfo.vue';
+// import CourseGoals from './components/CourseGoals.vue';
+import ActiveGoals from './components/ActiveGoals.vue';
+import ManageGoals from './components/ManageGoals.vue';
+
+export default {
+  components: {
+    // 'the-header': TheHeader
+    // TheHeader: TheHeader
+    TheHeader,
+    // BadgeList,
+    // UserInfo,
+    // CourseGoals,
+    ActiveGoals,
+    ManageGoals
+  },
+  data() {
+    return {
+      selectedComponent: 'active-goals',
+      activeUser: {
+        name: 'Maximilian Schwarzmüller',
+        description: 'Site owner and admin',
+        role: 'admin',
+      },
+    };
+  },
+  methods: {
+    setSelectedComponent(cmp) {
+      this.selectedComponent = cmp;
+    }
+  }
+};
+</script>
+
+<style>
+html {
+  font-family: sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>
