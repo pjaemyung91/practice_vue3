7fac6bea997c28748b0d55f9529d8976f88e6a14
Jaemyung <p.jaemyung91@gmail.com>
2024-03-18 17:02:07 +0900

    FEAT: Learn about Vuex

---
+++ vuex-01-starting-setup/src/components/FavoriteValue.vue
@@ -0,0 +1,17 @@
+<template>
+  <h3>{{ counter }}</h3>
+  <p>We do more...</p>
+</template>
+
+<script>
+export default {
+  computed: {
+    counter() {
+      // return this.$store.state.counter * 2;
+      // return this.$store.getters.finalCounter;
+      // return this.$store.getters.nomalizedCounter;
+      return this.$store.getters['numbers/nomalizedCounter'];
+    }
+  }
+}
+</script>
